ef094aae171e35298e01df7fdf9f61fb00b05e1f
nolife99 <89430659+nolife99@users.noreply.github.com>
2022-11-29 09:56:27 -0500

    wow
    keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes keyframes

--- Spectrum.cs
+++ Spectrum.cs
@@ -2,7 +2,7 @@ using OpenTK;
 using StorybrewCommon.Scripting;
 using StorybrewCommon.Storyboarding;
 using StorybrewCommon.Animations;
-using System;
+using static System.Math;
 
 namespace StorybrewScripts
 {
@@ -10,24 +10,21 @@ namespace StorybrewScripts
     {
         protected override void Generate()
         {
-		    MakeLinear(48949, 70577);
+            MakeLinear(48949, 70577);
             MakeLinear(126740, 145926);
             MakeLinear(151507, 173833);
             MakeLinear(258251, 279879);
         }
         void MakeLinear(int StartTime, int EndTime)
         {
-            var MinimalHeight = 1;
-            var Scale = new Vector2(17.5f, 100);
-            var LogScale = 15f;
-            var Position = new Vector2(-96, 240);
-            var Width = 856f;
+            var scale = new Vector2(17.5f, 100);
+            var basePos = new Vector2(-96, 240);
 
-            var BarCount = 35;
-            var fftCount = BarCount * 2;
+            var barCount = 35;
+            var fftCount = barCount * 2;
 
-            var heightKeyframes = new KeyframedValue<float>[fftCount];
-            for (var i = 0; i < fftCount; i++) heightKeyframes[i] = new KeyframedValue<float>(null);
+            var fftKeyframes = new KeyframedValue<float>[fftCount];
+            for (var i = 0; i < fftCount; i++) fftKeyframes[i] = new KeyframedValue<float>(null);
 
             var timeStep = Beatmap.GetTimingPointAt(StartTime).BeatDuration / 8;
             var offset = timeStep * 0.2;
@@ -37,26 +34,26 @@ namespace StorybrewScripts
                 var fft = GetFft(t + offset, fftCount, null, OsbEasing.InExpo);
                 for (var i = 0; i < fftCount; i++)
                 {
-                    var height = (float)Math.Pow(Math.Log10(1 + fft[i] * LogScale), 1.5) * Scale.Y;
-                    if (height < MinimalHeight) height = MinimalHeight;
+                    var height = Pow(Log10(1 + fft[i] * 15), 1.5) * scale.Y;
+                    if (height < 1) height = 1;
 
-                    heightKeyframes[i].Add(t, height);
+                    fftKeyframes[i].Add(t, (float)height);
                 }
             }
 
-            var barWidth = Width / BarCount;
-            for (var i = 0; i < BarCount; i++)
+            var barWidth = 856f / barCount;
+            for (var i = 0; i < barCount; i++)
             {
-                var keyframes = heightKeyframes[i];
-                keyframes.Simplify1dKeyframes(4, h => h);
+                var keyframe = fftKeyframes[i];
+                keyframe.Simplify1dKeyframes(4, h => h);
 
-                var bar = GetLayer("").CreateSprite("sb/p.png", OsbOrigin.Centre, new Vector2(Position.X + i * barWidth, Position.Y));
+                var bar = GetLayer("").CreateSprite("sb/p.png", OsbOrigin.Centre, new Vector2(basePos.X + i * barWidth, basePos.Y));
                 bar.Fade(StartTime, StartTime + 500, 0, .6);
                 bar.Fade(EndTime - 500, EndTime, .6, 0);
                 bar.Additive(StartTime);
 
-                keyframes.ForEachPair((start, end) => bar.ScaleVec(start.Time, end.Time, Scale.X, start.Value, Scale.X, end.Value),
-                    MinimalHeight, s => (int)s);
+                keyframe.ForEachPair((s, e) => bar.ScaleVec(s.Time, e.Time, scale.X, s.Value, scale.X, e.Value),
+                    1, s => (int)s);
             }
         }
     }
