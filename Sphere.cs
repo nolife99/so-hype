31889a3b561e21414fcf127a882ce7532a670ac2
nolife99 <89430659+nolife99@users.noreply.github.com>
2022-11-29 09:46:17 -0500

    static classes
    dededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdededdeded

--- Sphere.cs
+++ Sphere.cs
@@ -1,7 +1,9 @@
 using OpenTK;
 using StorybrewCommon.Scripting;
 using StorybrewCommon.Storyboarding;
-using System;
+using StorybrewCommon.Animations;
+using static OpenTK.MathHelper;
+using static System.Math;
 
 namespace StorybrewScripts
 {
@@ -9,11 +11,8 @@ namespace StorybrewScripts
     {
         protected override void Generate()
         {
-            var timer = System.Diagnostics.Stopwatch.StartNew();
             MakeSphere(73368, 92903, 140, 5, new Vector2(36, 20), 18, 900);
             MakeSphere(126740, 145926, 140, 5, new Vector2(36, 20), 15, 900);
-            timer.Stop();
-            Log($"Execution: {timer.ElapsedMilliseconds / 1000f} seconds");
         }
         void MakeSphere(int startTime, int endTime, double size, int split, Vector2 dots, double durationMul, int accuracyLevel)
         {
@@ -26,31 +25,31 @@ namespace StorybrewScripts
                 if (i > split && i < split * 2) break;
                 else if (i == split * 2) i = 1;
 
-                var rad = size * Math.Sin(c / dots.Y * Math.PI);
+                var rad = size * Sin(c / dots.Y * Math.PI);
                 var basePos = new Vector3d(
-                    rad * Math.Cos(r / dots.X * Math.PI * 2),
-                    size * Math.Cos(c / dots.Y * Math.PI),
-                    rad * Math.Sin(r / dots.X * Math.PI * 2));
-                        
-                var rotFunc = new Vector3d(MathHelper.DegreesToRadians(47.5), 0, MathHelper.DegreesToRadians(30));
+                    rad * Cos(r / dots.X * Math.PI * 2),
+                    size * Cos(c / dots.Y * Math.PI),
+                    rad * Sin(r / dots.X * Math.PI * 2));
+                
+                var rotFunc = new Vector3d(DegreesToRadians(47.5), 0, DegreesToRadians(30));
                 var pos = Vector3d.Transform(basePos, new Quaterniond(rotFunc));
 
                 var sprite = GetLayer("").CreateSprite("sb/d.png", OsbOrigin.Centre, new Vector2(0, (float)pos.Y + 240));
                 sprite.Fade(startTime + r * 40, startTime + r * 40 + 800, 0, 1);
                 sprite.Fade(endTime - r * 30, endTime - r * 30 + 800, 1, 0);
                 
-                var maxVal = new Vector2d();
+                var maxVal = new Keyframe<double>();
                 for (var f = .0; f < 360; f += 360 / (double)accuracyLevel)
                 {
-                    pos = Vector3d.Transform(basePos, new Quaterniond(rotFunc.X, MathHelper.DegreesToRadians(f), rotFunc.Z));
+                    pos = Vector3d.Transform(basePos, new Quaterniond(rotFunc.X, DegreesToRadians(f), rotFunc.Z));
                     if (pos.X < maxVal.Y) continue;
-                    maxVal = new Vector2d(spinDur / 360 * f, pos.X);
+                    maxVal = new Keyframe<double>(spinDur / 360 * f, pos.X);
                 }
 
-                var sTime = startTime + maxVal.X - spinDur;
-                sprite.StartLoopGroup(sTime, (int)Math.Ceiling((endTime + 1000 - sTime) / spinDur));
-                sprite.MoveX(OsbEasing.InOutSine, 0, spinDur / 2, 320 + maxVal.Y, 320 - maxVal.Y);
-                sprite.MoveX(OsbEasing.InOutSine, spinDur / 2, spinDur, 320 - maxVal.Y, 320 + maxVal.Y);
+                var sTime = startTime + maxVal.Time - spinDur;
+                sprite.StartLoopGroup(sTime, (int)Ceiling((endTime + 1000 - sTime) / spinDur));
+                sprite.MoveX(OsbEasing.InOutSine, 0, spinDur / 2, 320 + maxVal.Value, 320 - maxVal.Value);
+                sprite.MoveX(OsbEasing.InOutSine, spinDur / 2, spinDur, 320 - maxVal.Value, 320 + maxVal.Value);
                 sprite.EndGroup();
 
                 sprite.Scale(startTime, .03);
